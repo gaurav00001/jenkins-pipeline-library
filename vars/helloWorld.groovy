4d73d49d6aa89b07d5c683b5e4d9453649da3729
Gaurav <gkkumargaurav7@gmail.com>
2022-06-20 22:34:39 +0530

    Adding new file

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -5,6 +5,11 @@ import hudson.plugins.git.extensions.*
 import hudson.plugins.git.extensions.impl.*
 import jenkins.model.Jenkins
 
+def call(body){
+def config = [:]
+body.resolveStrategy = Closure.DELEGATE_FIRST
+body.delegate = config
+body()
 
 // parameters
 def jobParameters = [
