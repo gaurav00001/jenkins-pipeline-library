57253ea009d43edfb44a2f0ee6c717e4205554d9
Gaurav <44086655+gaurav00001@users.noreply.github.com>
2022-06-28 17:02:33 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,49 +1,7 @@
-#!groovy
-// imports
-import hudson.plugins.git.*
-import hudson.plugins.git.extensions.*
-import hudson.plugins.git.extensions.impl.*
-import jenkins.model.Jenkins
+#!/usr/bin/env groovy
+def jenkinsFile
+stage('Loading Jenkins file') {
+  jenkinsFile = fileLoader.fromGit('gaurav00001/Jenkinsfilefromremote/Jenkinsfile', 'https://github.com/gaurav00001/Jenkinsfilefromremote.git', 'master', null, '')
+}
 
-
-// parameters
-def jobParameters = [
-  name:          'MyJob',
-  description:   'Build of my STG environment : https://stg.mycompany.com',
-  repository:    'git@github.com:my-company/my-repo.git',
-  branch:        'master',
-  credentialId:  'jenkins-key'
-]
-
-// define repo configuration
-def branchConfig                =   [new BranchSpec(jobParameters.branch)]
-def userConfig                  =   [new UserRemoteConfig(jobParameters.repository, null, null, jobParameters.credentialId)]
-def cleanBeforeCheckOutConfig   =   new CleanBeforeCheckout()
-def sparseCheckoutPathConfig    =   new SparseCheckoutPaths([new SparseCheckoutPath("Jenkinsfile")])
-def cloneConfig                 =   new CloneOption(true, true, null, 3)
-def extensionsConfig            =   [cleanBeforeCheckOutConfig,sparseCheckoutPathConfig,cloneConfig]
-def scm                         =   new GitSCM(userConfig, branchConfig, false, [], null, null, extensionsConfig)
-
-// define SCM flow
-def flowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "Jenkinsfile")
-
-// set lightweight checkout
-flowDefinition.setLightweight(true)
-
-// get Jenkins instance
-Jenkins jenkins = Jenkins.getInstance()
-
-// create the job
-def job = new org.jenkinsci.plugins.workflow.job.WorkflowJob(jenkins,jobParameters.name)
-
-// define job type
-job.definition = flowDefinition
-
-// set job description
-job.setDescription(jobParameters.description)
-
-// save to disk
-jenkins.save()
-jenkins.reload()
-  
-
+jenkinsFile.start()
